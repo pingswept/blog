5ab1f552ee2d4f337e7496ac8e178ce517288903
Brandon Stafford <brandon@pingswept.org>
2024-01-04 19:04:57 -0500

    Start parsing posts, but poorly

--- make-pingswept-org.py
+++ make-pingswept-org.py
@@ -13,7 +13,20 @@ def isPage(filename):
     else:
         return False
 
-def formatRecipeForPrinting(basename, infile, outfile):
+def writePage(basename, infile, outfile):
+    print('Processing {0} into {1}'.format(infile.name, outfile.name))
+    html = markdown.markdown(infile.read(), extras=['metadata']).encode('utf8')
+    outfile.write(header)
+    outfile.write('<h2>{0}</h2>'.format(' '.join(basename.split('-')).capitalize()))
+    #for field in ['Date', 'Makes', 'Source', 'Time']:
+    #    try:
+    #        outfile.write('<div id="{0}">{0}: {1}</div>'.format(field, html.metadata[field]))
+    #    except KeyError:
+    #        pass
+    outfile.write(html.decode('utf-8'))
+    outfile.write('</body>\n</html>')
+
+def writePost(basename, infile, outfile):
     print('Processing {0} into {1}'.format(infile.name, outfile.name))
     html = markdown.markdown(infile.read(), extras=['metadata']).encode('utf8')
     outfile.write(header)
@@ -50,7 +63,17 @@ for file in sorted(filter(isPage, pagelist)):
     (basename, ext) = file.split('.')
     infile = codecs.open('pages/'+ file, 'r', 'utf-8')
     outfile = open('output/' + basename + '.html', 'w')
-    formatRecipeForPrinting(basename, infile, outfile)
+    writePage(basename, infile, outfile)
+    infile.close()
+    outfile.close()
+    print('Done with {0}'.format(infile.name))
+    print('')
+
+for file in sorted(filter(isPage, postlist)):
+    (basename, ext) = file.split('.')
+    infile = codecs.open('posts/'+ file, 'r', 'utf-8')
+    outfile = open('output/' + basename + '.html', 'w')
+    writePage(basename, infile, outfile)
     infile.close()
     outfile.close()
     print('Done with {0}'.format(infile.name))
